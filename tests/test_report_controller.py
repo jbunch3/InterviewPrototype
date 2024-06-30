1d7ddfcf31ad19aaa0701993fe3f9b913897bca3
Justin Bunch <jcbunch3@gmail.com>
2024-06-30 13:21:43 +0200

    CodeChalleng: missed file

---
+++ tests/test_report_controller.py
@@ -0,0 +1,17 @@
+import pytest
+import pdb
+
+
+from pcapp.report_controller import ReportController
+
+class TestReportGenerator:
+    def test_stanrdard_generator(test):
+        request = '{"request_name": "Monthly Report", "client": "1", "report_template": "standard", "report_metric": "performance", "asset_restrictons": "wind", "start": "2022-01-01", "debug": "true"}'
+
+        RunReport = ReportController()
+        RunReport.handle_request(request)
+        
+        assert RunReport
+
+
+
